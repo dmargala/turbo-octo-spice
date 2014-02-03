d52e6f539508175289789d35f4e2d0c88ac9f974
Daniel Margala <dmargala@uci.edu>
2014-02-03 13:49:48 -0800

    Add command line arguments to addLOS utility

--- addLOS.py
+++ addLOS.py
@@ -5,6 +5,7 @@
 import argparse
 import numpy
 
+lymanAlpha = 1216.
 
 def main():
     """Add fake line of sight data to qso position list"""
@@ -15,6 +16,14 @@ def main():
         help = "input file name")
     parser.add_argument("-o","--output", type = str, default = None,
         help = "output file name")
+    parser.add_argument("--forest-high", type = float, default = 1200.,
+        help = "Lyman-alpha forest high cutoff wavelength")
+    parser.add_argument("--forest-low", type = float, default = 1050.,
+        help = "Lyman-alpha forest low cutoff wavelength")
+    parser.add_argument("--spec-low", type = float, default = 3650.,
+        help = "Spectrograph wavelength lower-limit")
+    parser.add_argument("--combine", type = int, default = 3,
+        help = "Number of observed wavelengths bins to combine.")
     args = parser.parse_args()
 
     try:
@@ -23,20 +32,13 @@ def main():
     except Exception,e:
         raise RuntimeError('Failed to load file %r' % args.input)
 
-    lambdaObsMin = 3650.
-    lambdaAlphaCutoff = 1200.
-    lambdaBetaCutoff = 1050.
-
-    lambdaAlpha = 1216.
-
-    pixels = []
-    m = (10**(.0001))**3
+    m = (10**(.0001))**args.combine
 
     out = open(args.output, 'w')
 
     qsocounter = 0
     counter = 0
-
+    pixels = []
     for quasar in data:
         qsocounter += 1
         if (qsocounter % 10000) == 0:
@@ -44,15 +46,13 @@ def main():
         zquasar = quasar[2]
         if zquasar < 2:
             continue
-        zmin = max(lambdaObsMin/lambdaAlpha-1, lambdaBetaCutoff/lambdaAlpha*(1+zquasar)-1)
-        zmax = lambdaAlphaCutoff/lambdaAlpha*(1+zquasar)-1
+        zmin = max(args.spec_low/lymanAlpha-1, args.forest_low/lymanAlpha*(1+zquasar)-1)
+        zmax = args.forest_high/lymanAlpha*(1+zquasar)-1
         z = zmin
         while z < zmax:
             z = (1+z)*m-1
             out.write('%f %f %f\n' % (quasar[0], quasar[1], z))
             counter += 1
-
-
     out.close()
     print 'Wrote %d pixels to %s' % (counter,args.output)
 

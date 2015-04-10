4d37881d506238a329c2d205869f7d5e882a0250
Daniel Margala <dmargala@uci.edu>
2015-04-10 11:29:26 -0700

    update hdf5 delta reader to read updated delta file format

--- turbooctospice/HDF5Delta.cc
+++ turbooctospice/HDF5Delta.cc
@@ -46,10 +46,11 @@ local::HDF5Delta::HDF5Delta(std::string filename) : _filename(filename) {};
 std::vector<local::Forest> local::HDF5Delta::loadForests(int ncombine, float forestlo, float foresthi, float speclo, bool debug) {
     // open delta field file
     H5::H5File file(_filename, H5F_ACC_RDONLY);
-    H5::Group grp = file.openGroup("delta_field");
+    H5::Group grp = file.openGroup("lines_of_sight");
 
     static float flo(forestlo), fhi(foresthi), slo(speclo);
     static int n(ncombine), nforests(0);
+    static bool select_south_cap(debug);
 
     struct kludge { 
         static herr_t load_quasar(hid_t loc_id, const char *name, const H5L_info_t *linfo, void *opdata) {
@@ -114,47 +115,49 @@ std::vector<local::Forest> local::HDF5Delta::loadForests(int ncombine, float for
             H5::Group targetGroup;
             targetGroup = H5Gopen2(loc_id, name, H5P_DEFAULT);
 
-            if((nforests % 10000) == 0) std::cout << nforests << " : " << name << std::endl;
-
-            float ra, dec, z;
+            double ra, dec, z;
             readAttribute(targetGroup, "ra", ra);
             readAttribute(targetGroup, "dec", dec);
             readAttribute(targetGroup, "z", z);
 
-            // Read datasets
-            std::vector<float> delta, distance;
-            readDataSet(targetGroup, "delta", delta);
-            readDataSet(targetGroup, "distance", distance);
+            ra *= 180.0/PI;
+            dec *= 180.0/PI;
 
-            // init forest pixels
-            Forest forest;
-            double theta((90.0-dec)*DEG2RAD), phi(ra*DEG2RAD);
-            forest.phi = phi;
-            forest.theta = theta;
-            forest.sth = std::sin(theta);
-            forest.cth = std::cos(theta);
-            forest.sph = std::sin(phi);
-            forest.cph = std::cos(phi);
+            if(select_south_cap && !(ra < 120 || ra > 330)) {
+                if((nforests % 10000) == 0) std::cout << nforests << " : " << name << std::endl;
+    
+                // Read datasets
+                std::vector<float> delta, r_comov, weight, loglam;
+                readDataSet(targetGroup, "delta", delta);
+                readDataSet(targetGroup, "weight", weight);
+                readDataSet(targetGroup, "r_comov", r_comov);
+                readDataSet(targetGroup, "loglam", loglam);
 
-            // Iterate over pixels
-            float deltaSum, distanceSum, logLambda(std::log10(lyA*(1+z)));
-            for(int i = 0; i < delta.size()-n+1; i += n) {
-                deltaSum = 0; distanceSum = 0;
-                for(int j = 0; j < n; ++j) {
-                    deltaSum += delta[i+j];
-                    distanceSum += distance[i+j]; // ???
+                // init forest pixels
+                Forest forest;
+                double theta((90.0-dec)*DEG2RAD), phi(ra*DEG2RAD);
+                forest.phi = phi;
+                forest.theta = theta;
+                forest.sth = std::sin(theta);
+                forest.cth = std::cos(theta);
+                forest.sph = std::sin(phi);
+                forest.cph = std::cos(phi);
+
+                // Iterate over pixels
+                for(int i = 0; i < delta.size(); ++i) {
+                    // Save the pixel
+                    forest.pixels.push_back( {delta[i], loglam[i], weight[i], r_comov[i]} );
                 }
-                // Save the pixel
-                forest.pixels.push_back( {deltaSum/n, logLambda, 1.0, distanceSum/n} );
-            }
 
-            ((std::vector<Forest>*) opdata)->push_back(forest);
-            nforests++;
+                ((std::vector<Forest>*) opdata)->push_back(forest);
+                nforests++;
+            }
 
             targetGroup.close();
 
             return 0;
         }
+
     };
 
     std::vector<Forest> forests;

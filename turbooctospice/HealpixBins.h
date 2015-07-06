5538fb078298c1c91bf3e3792486afb9273d6416
Daniel Margala <djmargala@gmail.com>
2015-07-06 12:09:44 -0700

    clean up private variable names

--- turbooctospice/HealpixBins.h
+++ turbooctospice/HealpixBins.h
@@ -18,8 +18,8 @@ namespace turbooctospice {
         /// Create a new HealpixBins object
         /// @param order The HEALPix resolution order
         HealpixBins(int order) {
-            _map = HealpixMap(order, RING);
-            std::cout << "Max pix rad: " << _map.max_pixrad() << std::endl;
+            map_ = HealpixMap(order, RING);
+            std::cout << "Max pix rad: " << map_.max_pixrad() << std::endl;
         };
         /// Add an item's index to the bin containing the specified angular position
         /// @param ra Right ascension. The angular distance of a point east of the First Point of Aries, measured along the celestial equator, in radians (0 < ra < 2*pi).
@@ -29,17 +29,17 @@ namespace turbooctospice {
             // Find the healpix bin for this quasar and save it's index
             int binIndex(ang2pix(ra, dec));
 
-            if(_bins.count(binIndex) > 0) {
-                _bins[binIndex].push_back(item);
+            if(bins_.count(binIndex) > 0) {
+                bins_[binIndex].push_back(item);
             }
             else {
-                _bins[binIndex] = std::vector<T>(1, item);
-                _occupiedBins.push_back(binIndex);
+                bins_[binIndex] = std::vector<T>(1, item);
+                occupied_bins_.push_back(binIndex);
             }
-            ++_nentries;
+            ++num_entries_;
         };
         std::vector<int> getOccupiedBins() const {
-            return _occupiedBins;
+            return occupied_bins_;
         }
         /// Return bin indices within radius of an angular position
         /// @param ra Right ascension. The angular distance of a point east of the First Point of Aries, measured along the celestial equator, in radians (0 < ra < 2*pi).
@@ -48,26 +48,26 @@ namespace turbooctospice {
         /// @param fact HEALPix search precision parameter
         std::vector<int> getBinIndicesWithinRadius(double ra, double dec, double radius, int fact = 4) const {
             std::vector<int> neighbors;
-            _map.query_disc_inclusive(radec2pnt(ra, dec), radius, neighbors, fact);
+            map_.query_disc_inclusive(radec2pnt(ra, dec), radius, neighbors, fact);
             return neighbors;
         };
         /// Return true if the specified bin index has any contents
         /// @param index HealpixBins index
         bool checkBinExists(int index) const {
-            if(_bins.find(index) == _bins.end()) return false;
+            if(bins_.find(index) == bins_.end()) return false;
             return true;
         };
         /// Return the contents of a bin
         /// @param k Map key
         const typename Bins::mapped_type& getBinContents(const typename Bins::key_type& k) const {
-            return _bins.at(k);
+            return bins_.at(k);
         };
         /// Return the Healpix bin index containing the specified angular position
         /// @param ra Right ascension. The angular distance of a point east of the First Point of Aries, measured along the celestial equator, in radians (0 < ra < 2*pi).
         /// @param dec Declination. The angular distance of a point north or south of the celestial equator, in radians (-pi/2 < dec < pi/2).
         int ang2pix(double ra, double dec) const {
             try {
-                return _map.ang2pix(radec2pnt(ra, dec));
+                return map_.ang2pix(radec2pnt(ra, dec));
             }
             catch(PlanckError e) {
                 std::cerr << "HealBins.ang2pix: " << e.what() << std::endl;
@@ -81,24 +81,24 @@ namespace turbooctospice {
             return {0.5*pi - dec, ra};
         }
         /// Return the number of bins that contain at least one item
-        int getNBins() const { return _bins.size(); };
+        int getNBins() const { return bins_.size(); };
         /// Return the total number of entries.
-        unsigned long getNEntries() const { return _nentries; };
+        unsigned long getNEntries() const { return num_entries_; };
         /// Print bin center of specified index
         void printBinCenter(const int index) const {
-            auto p = _map.pix2ang(index);
+            auto p = map_.pix2ang(index);
             std::cout << 0.5*pi - p.theta << " " << p.phi;
         }
     private:
-        int _order;
-        Bins _bins;
-        HealpixMap _map;
-        unsigned long _nentries;
-        std::vector<int> _occupiedBins;
+        int order_;
+        Bins bins_;
+        HealpixMap map_;
+        unsigned long num_entries_;
+        std::vector<int> occupied_bins_;
     }; // HealpixBins
 
     typedef HealpixBins<int> HealpixBinsI;
 
 } // turbooctospice
 
-#endif
+#endif // TURBOOCTOSPICE_HEALPIX_BINS

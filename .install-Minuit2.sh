37496988a7b2f2a650494633a9086ed81385373b
Daniel Margala <djmargala@gmail.com>
2016-02-14 14:10:45 -0800

    add Minuit2 install

---
+++ .install-Minuit2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -ex
+wget http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz
+tar -xzvf Minuit2-5.34.14.tar.gz
+cd Minuit2-5.34.14 && ./configure --disable-openmp --prefix=/usr && make && sudo make install
+
+
+# curl -k -L http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz | tar zx
+# cd Minuit2-5.34.14
+# ./configure --disable-openmp --prefix=/usr/local
+# make
+# sudo make install

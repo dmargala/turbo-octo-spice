2b88fe8762c722c38beaa5249eabfa87e7b3aa15
Daniel Margala <djmargala@gmail.com>
2016-02-14 14:17:11 -0800

    use curl instead of wget

--- .install-Minuit2.sh
+++ .install-Minuit2.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -ex
-wget http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz
-tar -xzvf Minuit2-5.34.14.tar.gz
+curl -k -L http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz | tar zx
 cd Minuit2-5.34.14 && ./configure --disable-openmp --prefix=/usr && make && sudo make install
 
 

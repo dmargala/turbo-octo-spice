c00c1f80acca0737eb5357f0c16ce497a27d9692
Daniel Margala <djmargala@gmail.com>
2016-02-14 17:11:21 -0800

    dont install Healpix

--- install-Healpix.sh
+++ install-Healpix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 wget http://downloads.sourceforge.net/project/healpix/Healpix_3.30/Healpix_3.30_2015Oct08.tar.gz
-tar -xzvf Healpix_3.30_2015Oct08.tar.gz
-cd Healpix_3.30/src/cxx && autoconf &&./configure --prefix=/usr && make && sudo make install
+tar -xzf Healpix_3.30_2015Oct08.tar.gz
+cd Healpix_3.30/src/cxx && autoconf && ./configure && make

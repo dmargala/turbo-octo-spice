cf7de1f07e2c7328a95ff2c22d0b8dc94858ec4e
Daniel Margala <djmargala@gmail.com>
2016-02-14 17:03:34 -0800

    minuit isnt working now

--- install-Healpix.sh
+++ install-Healpix.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -ex
-curl -k -L http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz | tar zx
-cd Minuit2-5.34.14 && ./configure --disable-openmp --prefix=/usr && make && sudo make install
-
-
-# curl -k -L http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz | tar zx
-# cd Minuit2-5.34.14
-# ./configure --disable-openmp --prefix=/usr/local
-# make
-# sudo make install
+wget http://downloads.sourceforge.net/project/healpix/Healpix_3.30/Healpix_3.30_2015Oct08.tar.gz
+tar -xzvf Healpix_3.30_2015Oct08.tar.gz
+cd Healpix_3.30/src/cxx && autoconf &&./configure --prefix=/usr && make && sudo make install

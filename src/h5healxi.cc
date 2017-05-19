f87b10d2dfcb7c52af1adbfe494964996b037e56
Daniel Margala <djmargala@gmail.com>
2017-05-19 15:01:20 -0700

    clean up h5healxi

--- src/h5healxi.cc
+++ src/h5healxi.cc
@@ -15,6 +15,48 @@ namespace po = boost::program_options;
 namespace lk = likely;
 namespace tos = turbooctospice;
 
+
+void printGridConfig(tos::AbsTwoPointGridPtr const &grid) {
+    // Prints something like:
+    //
+    // Binning grid configuration:
+    //  axis1 : 0 200 4 50
+    //  axis2 : 0 200 4 50
+    //  axis3 : 3.556 3.735 0.179 1
+    std::cout << "Binning grid configuration: " << std::endl;
+    for(int axis = 0; axis < 3; ++axis) {
+        std::cout << " axis" << axis + 1
+            << " : " << grid->getAxisMin(axis)
+            << " " << grid->getAxisMax(axis)
+            << " " << grid->getAxisBinWidth(axis)
+            << " " << grid->getAxisNBins(axis)
+            << std::endl;
+    }
+}
+
+
+double loglamToRedshift(double const &loglam, double const &loglam0) {
+    return std::pow(10, loglam - loglam0) - 1;
+}
+
+// axis1
+std::string axis_rpara("[0:200]*50");
+std::string axis_r("[0:200]*50");
+std::string axis_dloglam("[0:0.05]*25");
+
+// axis2
+std::string axis_rperp("[0:200]*50");
+std::string axis_mu("[0:1]*1");
+std::string axis_dtheta("[5:175]*1");
+
+// axis3
+std::string axis_log_lya_one_plus_z("[3.556:3.735]*1");
+std::string axis_log_one_plus_z("[0.46:.65]*1");
+
+// darkmatter platelist path
+std::string dm_platelist("/data/sas/dr12/boss/spectro/redux/v5_7_0/platelist.fits");
+
+
 int main(int argc, char **argv) {
 
     int order, nthreads, num_sightlines;
@@ -36,13 +78,10 @@ int main(int argc, char **argv) {
             "Filename to read from")
         ("output,o", po::value<std::string>(&outfile)->default_value("healxi"),
             "Output filename base")
-        //("axis1", po::value<std::string>(&axis1)->default_value("[0:0.05]*25"),
         ("axis1", po::value<std::string>(&axis1)->default_value("[0:200]*50"),
             "Axis-1 binning, r_par (Mpc/h), r (Mpc/h), or dloglam")
-        //("axis2", po::value<std::string>(&axis2)->default_value("[5:175]*1"),
         ("axis2", po::value<std::string>(&axis2)->default_value("[0:1]*1"),
             "Axis-2 binning, r_perp (Mpc/h), mu (r_par/r), or dtheta (arcmin)")
-        // ("axis3", po::value<std::string>(&axis3)->default_value("[0.46:.65]*1"),
         ("axis3", po::value<std::string>(&axis3)->default_value("[3.556:3.735]*1"),
             "Axis-3 binning, log10(lya*(1+z))")
         ("polar", "(r,mu,z) binning")
@@ -51,7 +90,7 @@ int main(int argc, char **argv) {
         ("save-subsamples", "save xi estimates on individual subsamples")
         ("num-sightlines,n", po::value<int>(&num_sightlines)->default_value(0),
             "number of sightlines to use.")
-        ("platelist-filename", po::value<std::string>(&platelist_filename)->default_value("/data/sas/dr12/boss/spectro/redux/v5_7_0/platelist.fits"),
+        ("platelist-filename", po::value<std::string>(&platelist_filename)->default_value(dm_platelist),
             "platelist filename")
         ;
     // do the command line parsing now
@@ -60,16 +99,20 @@ int main(int argc, char **argv) {
         po::store(po::parse_command_line(argc, argv, cli), vm);
         po::notify(vm);
     }
-    catch(std::exception const &e) {
-        std::cerr << "Unable to parse command line options: " << e.what() << std::endl;
+    catch (std::exception const &e) {
+        std::cerr << "Unable to parse command line options: "
+            << e.what() << std::endl;
         return -1;
     }
-    if(vm.count("help")) {
+    if (vm.count("help")) {
         std::cout << cli << std::endl;
         return 1;
     }
-    bool verbose(vm.count("verbose")), polar(vm.count("polar")), cart(vm.count("cart")),
-        debug(vm.count("debug")), save_subsamples(vm.count("save-subsamples"));
+    bool verbose(vm.count("verbose"));
+    bool debug(vm.count("debug"));
+    bool polar(vm.count("polar"));
+    bool cart(vm.count("cart"));
+    bool save_subsamples(vm.count("save-subsamples"));
 
     if (infile.size() == 0) {
         std::cerr << "Must specify input file." << std::endl;
@@ -78,11 +121,11 @@ int main(int argc, char **argv) {
 
     // set up cosmology
     cosmo::AbsHomogeneousUniversePtr cosmology;
-    if(OmegaMatter == 0) OmegaMatter = 1 - OmegaLambda;
+    if (OmegaMatter == 0) OmegaMatter = 1 - OmegaLambda;
     try {
         cosmology.reset(new cosmo::LambdaCdmUniverse(OmegaLambda, OmegaMatter));
     }
-    catch(cosmo::RuntimeError const &e) {
+    catch (cosmo::RuntimeError const &e) {
         std::cerr << e.what() << std::endl;
         return -1;
     }
@@ -92,12 +135,15 @@ int main(int argc, char **argv) {
     tos::XiEstimator::BinningCoordinateType type;
 
     try {
-        lk::AbsBinningCPtr bins1 = lk::createBinning(axis1), bins2 = lk::createBinning(axis2), bins3 = lk::createBinning(axis3);
-        if(polar) {
+        lk::AbsBinningCPtr bins1 = lk::createBinning(axis1);
+        lk::AbsBinningCPtr bins2 = lk::createBinning(axis2);
+        lk::AbsBinningCPtr bins3 = lk::createBinning(axis3);
+
+        if (polar) {
             type = tos::XiEstimator::PolarCoordinates;
             grid.reset(new tos::PolarGrid(bins1, bins2, bins3));
         }
-        else if(cart) {
+        else if (cart) {
             type = tos::XiEstimator::CartesianCoordinates;
             grid.reset(new tos::CartesianGrid(bins1, bins2, bins3));
         }
@@ -105,27 +151,36 @@ int main(int argc, char **argv) {
             type = tos::XiEstimator::ObservingCoordinates;
             grid.reset(new tos::QuasarGrid(bins1, bins2, bins3));
         }
-        if(verbose) {
-            std::cout << "Binning grid configuration: " << std::endl;
-            for(int axis = 0; axis < 3; ++axis) {
-                std::cout << " axis" << axis+1 << " : " << grid->getAxisMin(axis) << " " << grid->getAxisMax(axis) << " "
-                    << grid->getAxisBinWidth(axis) << " " << grid->getAxisNBins(axis) << std::endl;
-            }
+        if (verbose) {
+            printGridConfig(grid);
         }
     }
-    catch(likely::RuntimeError const &e) {
+    catch (lk::RuntimeError const &e) {
         std::cerr << e.what() << std::endl;
         return -1;
     }
 
     try {
+        // initialize sky bins
+        tos::SkyBinsIPtr skybins;
+        // which sky binning are we using?
+        if(order > 0) {
+            skybins.reset(new tos::HealpixBinsI(order));
+        }
+        else {
+            // should check that the filename is valid...
+            // the default value is a hard coded pather on darkmatter
+            skybins.reset(new tos::PlateBinsI(platelist_filename));
+        }
+
         // load forest sight lines
         tos::HDF5Delta file(infile);
         if (verbose) {
-            std::cout << "Loading forest delta fields from " << infile << std::endl;
+            std::cout << "Loading forest delta fields from "
+                << infile << std::endl;
         }
         std::vector<tos::Forest> sightlines(file.loadForests("lines_of_sight"));
-        // trim sample, if requested
+        // trim number of sightlines to use, if requested
         if(num_sightlines > 0 && num_sightlines < sightlines.size()){
             std::vector<tos::Forest>::const_iterator first = sightlines.begin();
             std::vector<tos::Forest>::const_iterator last = sightlines.begin() + num_sightlines;
@@ -134,29 +189,18 @@ int main(int argc, char **argv) {
         }
         num_sightlines = sightlines.size();
 
-        // create sky bins
-        tos::SkyBinsIPtr skybins;
-        // which sky binning are we using?
-        if(order > 0) {
-            skybins.reset(new tos::HealpixBinsI(order));
-        }
-        else {
-            skybins.reset(new tos::PlateBinsI(platelist_filename));
-        }
-
         // add sightlines to sky bins
         unsigned num_pixels(0);
-        double min_loglam(sightlines[0].pixels[0].loglam), max_loglam(sightlines[0].pixels[0].loglam);
+        double min_loglam(sightlines[0].pixels[0].loglam);
+        double max_loglam(sightlines[0].pixels[0].loglam);
         for(auto &sightline : sightlines) {
             int npixels = sightline.pixels.size();
             num_pixels += npixels;
-            // std::cout << sightline.plate << ' ' << sightline.ra << ' ' << sightline.dec << std::endl;
-            // if(sightline.pixels[0].distance == 0) {
+            // compute distance to pixels in sightline
             for(auto &pixel : sightline.pixels){
-                pixel.distance = cosmology->getLineOfSightComovingDistance(std::pow(10, pixel.loglam-tos::logLyA)-1);
+                pixel.distance = cosmology->getLineOfSightComovingDistance(
+                    loglamToRedshift(pixel.loglam, tos::logLyA));
             }
-            // }
-
             // add sightline to sky bins
             skybins->addItem(sightline, sightline.forest_id);
             // find minimum loglam
@@ -171,44 +215,81 @@ int main(int argc, char **argv) {
             }
         }
 
-        // sightline/pixel stats
-        std::cout << "Loaded " << num_pixels << " pixels from " << num_sightlines << " lines of sight (LOS)" << std::endl;
         if(verbose) {
-            double avg_pixels_per_los(static_cast<double>(num_pixels)/num_sightlines);
-            std::cout << "Average number of pixels per LOS: " << boost::lexical_cast<std::string>(avg_pixels_per_los) << std::endl;
+            // sightline/pixel stats
+            std::cout << "Loaded " << num_pixels
+                << " pixels from " << num_sightlines
+                << " lines of sight (LOS)" << std::endl;
+
+            double avg_pixels_per_los(
+                static_cast<double>(num_pixels)/num_sightlines);
+            std::cout << "Average number of pixels per LOS: "
+                << boost::lexical_cast<std::string>(avg_pixels_per_los)
+                << std::endl;
+
             unsigned num_skybins_occupied(skybins->getNBins());
-            std::cout << "Number of sky bins occupied: " << num_skybins_occupied << std::endl;
+            std::cout << "Number of sky bins occupied: "
+                << num_skybins_occupied
+                << std::endl;
         }
+
         // the minimum redshift sets the angular scale we will need to consider
-        double zmin(std::pow(10, min_loglam-tos::logLyA)-1);
-        double max_transverse_scale(cosmology->getTransverseComovingScale(zmin));
+        double zmin(loglamToRedshift(min_loglam, tos::logLyA));
+        double max_transverse_scale(
+            cosmology->getTransverseComovingScale(zmin));
+
         if(verbose) {
             double max_ang(grid->maxAngularScale(max_transverse_scale));
-            std::cout << "Transverse comoving scale at z = " << boost::lexical_cast<std::string>(zmin) <<  " : "
-                << boost::lexical_cast<std::string>(max_transverse_scale) << " (Mpc/h)" << std::endl;
-            std::cout << "Max angular scale at z = " << boost::lexical_cast<std::string>(zmin) <<  " : "
-                << boost::lexical_cast<std::string>(max_ang) << " (rad)" << std::endl;
-            // for curiosity's sake
-            double zmax(std::pow(10, max_loglam-tos::logLyA)-1);
-            double min_transverse_scale(cosmology->getTransverseComovingScale(zmax));
+            std::cout << "Transverse comoving scale at z = "
+                << boost::lexical_cast<std::string>(zmin)
+                <<  " : "
+                << boost::lexical_cast<std::string>(max_transverse_scale)
+                << " (Mpc/h)"
+                << std::endl;
+            std::cout << "Max angular scale at z = "
+                << boost::lexical_cast<std::string>(zmin)
+                <<  " : "
+                << boost::lexical_cast<std::string>(max_ang)
+                << " (rad)"
+                << std::endl;
+
+            // for curiosity's sake compute at scales at max_loglam
+            double zmax(loglamToRedshift(max_loglam, tos::logLyA));
+            double min_transverse_scale(
+                cosmology->getTransverseComovingScale(zmax));
+
             double min_ang(grid->maxAngularScale(min_transverse_scale));
-            std::cout << "Transverse comoving scale at z = " << boost::lexical_cast<std::string>(zmax) <<  " : "
-                << boost::lexical_cast<std::string>(min_transverse_scale) << " (Mpc/h)" << std::endl;
-            std::cout << "Max angular scale at z = " << boost::lexical_cast<std::string>(zmax) <<  " : "
-                << boost::lexical_cast<std::string>(min_ang) << " (rad)" << std::endl;
+            std::cout << "Transverse comoving scale at z = "
+                << boost::lexical_cast<std::string>(zmax)
+                <<  " : "
+                << boost::lexical_cast<std::string>(min_transverse_scale)
+                << " (Mpc/h)"
+                << std::endl;
+            std::cout << "Max angular scale at z = "
+                << boost::lexical_cast<std::string>(zmax)
+                <<  " : "
+                << boost::lexical_cast<std::string>(min_ang)
+                << " (rad)"
+                << std::endl;
+
             std::cout << std::endl;
         }
 
+        // initialize estimator
+        tos::XiEstimator xiest(
+            max_transverse_scale, grid, type, sightlines, skybins);
+
         // run the estimator
-        tos::XiEstimator xiest(max_transverse_scale, grid, type, sightlines, skybins);
         xiest.run(nthreads);
         std::cout << std::endl;
+
+        // save the results
         xiest.save_results(outfile);
-        if(save_subsamples) {
+        if (save_subsamples) {
             xiest.save_subsamples(outfile);
         }
     }
-    catch(tos::RuntimeError const &e) {
+    catch (tos::RuntimeError const &e) {
         std::cerr << e.what() << std::endl;
         return -1;
     }
